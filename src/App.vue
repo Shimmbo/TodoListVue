b6436d5d3b6dd540ebedf041dfc9268d6fffc3fe
DESKTOP-K89MI50\Jimmy <jaimeduarte2411@hotmail.com>
2019-03-04 16:31:09 -0600

    ToDo functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,149 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="todolist not-done">
+          <h1>Todos</h1>
+          <div class="input-group">
+            <input v-model="newTask" type="text" class="form-control add-todo" placeholder="Add todo">
+            <span class="input-group-btn">
+              <button @click="addToList()" class="btn btn-default">Add</button>
+            </span>
+          </div>
+          <hr>
+          <ul class="list-unstyled">
+            <TodoListItem
+              v-for="todo in allTodoList"
+              :key="todo.Id"
+              :todoItem="todo"
+              @remove='removeTodo'
+            />
+          </ul>
+          <div class="todo-footer">
+              <strong><span class="count-todos"></span></strong> Items Left: {{allTodoList.length}}
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+          <div class="todolist">
+            <h1>Already Done</h1>
+            <ul id="done-items"  class="list-unstyled"> 
+              <TodoListItem
+                v-for="todo in completedList"
+                :key="todo.Id"
+                :todoItem="todo"
+                @remove='removeTodo'
+              />
+            </ul>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoListItem from './components/TodoListItem.vue'
 
 export default {
-  name: 'app',
+  mounted(){
+    if (localStorage.getItem('TODOS-LIST')) 
+      this.todoList = JSON.parse(localStorage.getItem('TODOS-LIST'));
+  },
+  data(){
+    return {
+      newTask: '',
+      todoList: [],
+    }
+  },
+  methods:{
+    addToList(){
+      if(this.newTask == '') return;
+
+      var nextId = this.getNextId();
+      nextId++;
+      this.todoList.push({
+        Id: nextId,
+        Description: this.newTask,
+        Completed: false
+      });
+      this.newTask = '';
+    },
+    getNextId(){
+      var max = 0;
+      for(var i = 0; i < this.todoList.length; i++){
+        max = Math.max(this.todoList[i].Id, max);
+      }
+      return max;
+    },
+    removeTodo(todoItem){
+      this.todoList = this.todoList.filter(function(x) {
+        return x.Id != todoItem.Id;
+      })
+    }
+  },
+  computed: {
+    allTodoList(){
+      return this.todoList.filter(function(x) {return !x.Completed})
+    },
+    completedList(){
+       return this.todoList.filter(function(x) {return x.Completed})
+    }
+  },
+  watch: {
+    todoList: {
+      handler() {
+        localStorage.setItem('TODOS-LIST', JSON.stringify(this.todoList));
+      },
+      deep: true,
+    },
+  },
   components: {
-    HelloWorld
+    TodoListItem
   }
 }
 </script>
+<style scoped>
+body{
+    background-color:#EEEEEE;
+}
+.todolist{
+    background-color:#FFF;
+    padding:20px 20px 10px 20px;
+    margin-top:30px;
+}
+.todolist h1{
+    margin:0;
+    padding-bottom:20px;
+    text-align:center;
+}
+.form-control{
+    border-radius:0;
+}
+li.ui-state-default{
+    background:#fff;
+    border:none;
+    border-bottom:1px solid #ddd;
+}
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+li.ui-state-default:last-child{
+    border-bottom:none;
+}
+
+.todo-footer{
+    background-color:#F4FCE8;
+    margin:0 -20px -10px -20px;
+    padding: 10px 20px;
+}
+#done-items li{
+    padding:10px 0;
+    border-bottom:1px solid #ddd;
+    text-decoration:line-through;
+}
+#done-items li:last-child{
+    border-bottom:none;
+}
+#checkAll{
+    margin-top:10px;
 }
 </style>
+
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <li class="ui-state-default">
+    <div class="checkbox">
+        <div class="input-group">
+            <label v-if="!isEditing" class="form-control" style="border:0px">
+                <input @change="$emit('changeStatus', todoItem)" type="checkbox" name="Completed" v-model="todoItem.Completed">
+                {{todoItem.Description}}
+            </label>
+            <input v-if="isEditing" type="text" class="form-control" v-model="todoItem.Description">
+            <span class="input-group-btn float-right">
+                <button v-if="!isEditing" @click="setEditing(true)" class="btn btn-primary">Edit</button>
+                <button v-if="!isEditing" @click="$emit('remove', todoItem)" class="btn btn-danger">Remove</button>
+                <button v-if="isEditing" @click="setEditing(false)" class="btn btn-success">Save</button>
+                <button v-if="isEditing" @click="setEditing(false)" class="btn btn-warning">Cancel</button>
+            </span>
+        </div>
+    </div>
+    
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todoItem: {
+        type: Object,
+        required: true
+    }
+  },
+  data(){
+      return {
+          isEditing: false,
+      }
+  },
+  methods: {
+      setEditing(isEditing){
+          this.isEditing = isEditing;
+      }
+  }
+}
+</script>
